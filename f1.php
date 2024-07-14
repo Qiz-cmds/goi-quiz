55944f14c3a9f5660b43b024b06451ef5e4d1b42
Qiz-cmds <aiharayuzu490@gmail.com>
2024-07-14 09:24:15 +0800

    Add files via upload

---
+++ f1.php
@@ -0,0 +1,152 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>f1</title>
+</head>
+<body class="shrek">
+<audio autoplay loop>
+        <source src="audio/game.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>    
+    <div class="container0">
+    <div class="w3-animate-zoom">
+        <img class="images1" src="image/volley.png" class="w3-round" style="width:20%">
+        </div>
+            
+        
+            <div class="w3-animate-zoom">
+                <img class="images2" src="image/nothing.png" class="w3-round" style="width:20%">
+                </div>
+                    
+                
+                
+                    <div class="w3-animate-zoom">
+                        <img class="images3" src="image/amin.png" class="w3-round" style="width:20%">
+                        </div>
+                           
+                        
+                        
+                            <div class="w3-animate-zoom">
+                                <img class="images4" src="image/fam.png" class="w3-round" style="width:22%">
+                                </div>
+                                   
+                               
+                                
+                                    <div class="w3-animate-zoom">
+                                        <img class="images5" src="image/work.png" class="w3-round" style="width:20%">
+                                        </div>
+                                           
+                                        </div>
+
+                                        <!--BUTTONS-->
+                                        <div>
+                                            <a href="secpage.html">
+                                            <button class="buttonf1"><b><i>BACK</i></b></button>
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="f1-g&m.php?button=1">
+                                            <button name="button" class="buttonf1-a" value="1" type="button"><b><i>CHAPTER 1</i></b></button>
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="f1-g&m.php?button=2">
+                                            <button  name="button" class="buttonf1-b" value="2"><b><i>CHAPTER 2</i></b></button>
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="f1-g&m.php?button=3">
+                                            <button  name="button" class="buttonf1-c" value="3"><b><i>CHAPTER 3</i></b></button>
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="f1-g&m.php?button=4">
+                                            <button  name="button" class="buttonf1-d" value="4"><b><i>CHAPTER 4</i></b></button>
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="f1-g&m.php?button=5">
+                                            <button  name="button" class="buttonf1-e" value="5"><b><i>CHAPTER 5</i></b></button>
+                                            </a>
+                                        </div>
+                                    </div>
+
+</body>
+<style>
+    .shrek{ 
+        background-image: url('image/jepun.jpg');
+        background-size: cover;
+
+}
+.buttonf1-a{
+    border-radius : 20px;
+    color:rgb(234, 59, 59);
+    font-size:40px;
+    width:17%;
+    height:7%;
+    position: absolute; /* Set button to absolute position */
+   top: 41%; /* Position button at vertical center */
+   left: 10%; /* Position button at horizontal center */
+  
+   transform: translate(-50%, -5%); /* Center button exactly */  
+
+}
+.buttonf1-b{
+    border-radius : 20px;
+    color:rgb(234, 59, 59);
+    font-size:40px;
+    width:19%;
+    height:7%;
+    position: absolute; /* Set button to absolute position */
+   top: 50%; /* Position button at vertical center */
+   left: 11%; /* Position button at horizontal center */
+  
+   transform: translate(-50%, 490%); /* Center button exactly */  
+
+}
+.buttonf1-c{
+    border-radius : 20px;
+    color:rgb(234, 59, 59);
+    font-size:40px;
+    width:19%;
+    height:7%;
+    position: absolute; /* Set button to absolute position */
+   top: 50%; /* Position button at vertical center */
+   left: 50%; /* Position button at horizontal center */
+  
+   transform: translate(-50%, 260%); /* Center button exactly */  
+
+}
+.buttonf1-d{
+    border-radius : 20px;
+    color:rgb(234, 59, 59);
+    font-size:40px;
+    width:17%;
+    height:7%;
+    position: absolute; /* Set button to absolute position */
+   top: 43%; /* Position button at vertical center */
+   left: 90%; /* Position button at horizontal center */
+  
+   transform: translate(-50%, 5%); /* Center button exactly */  
+
+}
+.buttonf1-e{
+    border-radius : 20px;
+    color:rgb(234, 59, 59);
+    font-size:40px;
+    width:18%;
+    height:7%;
+    position: absolute; /* Set button to absolute position */
+   top: 50%; /* Position button at vertical center */
+   left: 90%; /* Position button at horizontal center */
+  
+   transform: translate(-50%, 490%); /* Center button exactly */  
+
+}
+</style>
+
+</html>
